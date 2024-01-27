801fe1de6ae34c6ab3efe3681098597087256e2b
garden <gkdlqkql6@naver.com>
2024-01-28 02:46:06 +0900

    chore : 작업실 생성 시 해당 작업실 PK 제공하도록 변경

--- backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/application/WorkspaceService.java
+++ backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/application/WorkspaceService.java
@@ -6,6 +6,6 @@ import com.geeks.letsnote.domain.studio.workSpace.dto.ResponseWorkspaces;
 import java.util.List;
 
 public interface WorkspaceService {
-    List<ResponseWorkspaces.WorkspaceDto> getAllWorkspacesByUserName(String userName);
-    String createWorkspace(RequestWorkspaces.WorkspaceDto workspaceDto);
+    List<ResponseWorkspaces.WorkspaceDto> getAllWorkspacesByOwnerId(Long accountId);
+    ResponseWorkspaces.WorkspaceId createWorkspace(RequestWorkspaces.WorkspaceDto workspaceDto, Long accountId);
 }
