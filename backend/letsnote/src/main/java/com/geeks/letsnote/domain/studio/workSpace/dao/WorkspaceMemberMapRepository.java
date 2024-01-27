f0b384e2f3afdf9794cb51fd49fddafd0b477cc6
garden <gkdlqkql6@naver.com>
2024-01-28 02:46:51 +0900

    fix : 작업실 정보 중 member의 nickname을 가져오는 로직 변경

--- backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/dao/WorkspaceMemberMapRepository.java
+++ backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/dao/WorkspaceMemberMapRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WorkspaceMemberMapRepository extends JpaRepository<WorkspaceMemberMap,Long> {
-    List<Long> findAccountIdsBySpaceId(String spaceId);
+    List<WorkspaceMemberMap> findAllBySpaceId(String spaceId);
 }
