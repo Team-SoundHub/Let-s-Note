4b2ea6b0a5fda74c2e090a279e61860ee8fa6902
김민규 <wls8146com@gmail.com>
2024-01-29 22:44:16 +0900

    feat: write down some query for workspace loading

--- backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/dao/WorkspaceMemberMapRepository.java
+++ backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/dao/WorkspaceMemberMapRepository.java
@@ -2,11 +2,19 @@ package com.geeks.letsnote.domain.studio.workSpace.dao;
 
 import com.geeks.letsnote.domain.studio.workSpace.entity.WorkspaceMemberMap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface WorkspaceMemberMapRepository extends JpaRepository<WorkspaceMemberMap,Long> {
-    List<WorkspaceMemberMap> findAllBySpaceId(String spaceId);
 
-    List<WorkspaceMemberMap> findAllByAccountId(Long accountId);
+    @Query("SELECT w.spaceId FROM WorkspaceMemberMap w WHERE w.accountId = :accountId")
+    List<String> findSpaceIdsByAccountId(@Param("accountId")Long accountId);
+
+//    @Query("SELECT w.accountId FROM WorkspaceMemberMap w WHERE w.spaceId = :spaceId AND w.accountId != :ownerId")
+//    List<Long> findAccountIdsBySpaceIdNotInOwnerId(@Param("spaceId")String spaceId, @Param("ownerId")Long ownerId);
+
+    @Query("SELECT w.accountId FROM WorkspaceMemberMap w WHERE w.spaceId = :spaceId")
+    List<Long> findAccountIdsBySpaceId(@Param("spaceId")String spaceId);
 }
--- backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/dao/WorkspaceRepository.java
+++ backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/dao/WorkspaceRepository.java
@@ -2,6 +2,8 @@ package com.geeks.letsnote.domain.studio.workSpace.dao;
 
 import com.geeks.letsnote.domain.studio.workSpace.entity.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace,String> {
     List<Workspace> findAllByOwnerId(Long ownerId);
+
+    @Query("SELECT w.spaceId FROM Workspace w WHERE w.ownerId IN :accountIds ORDER BY w.updateAt")
+    List<Workspace> findSpaceIdsByOwnerIdsOrderByUpdateAt(@Param("accountIds")List<String> accountIds);
 }
