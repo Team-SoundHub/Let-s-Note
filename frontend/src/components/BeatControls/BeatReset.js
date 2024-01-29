cddfc14d63fac08d3af42777002f97e5e92b8c56
1stApplePie <chanwoo6422@naver.com>
2024-01-30 01:44:23 +0900

    feat/63-정지 버튼 구현

--- frontend/src/components/BeatControls/BeatReset.js
+++ frontend/src/components/BeatControls/BeatReset.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import BeatButton from './BeatButton'
+import React from "react";
+import styled from "styled-components";
+import BeatButton from "./BeatButton";
 
 const ResetIcon = styled.i`
   font-size: 24px;
   color: #363636;
-`
+`;
 
 const BeatReset = ({ onClick }) => (
   <BeatButton onClick={onClick}>
-    <ResetIcon className='fas fa-redo-alt' />
+    <ResetIcon className="fas fa-redo-alt" />
+    리셋
   </BeatButton>
-)
+);
 
-export default BeatReset
+export default BeatReset;
