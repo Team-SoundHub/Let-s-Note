ffea76d8073248839c98fbbdc254f43c03db0484
garden <gkdlqkql6@naver.com>
2024-02-02 21:30:46 +0900

    merge : merge commit

--- frontend/src/containers/workplace/WorkSpaceContainer.js
+++ frontend/src/containers/workplace/WorkSpaceContainer.js
@@ -149,6 +149,53 @@ class WorkSpaceContainer extends Component {
     }
   };
 
+  setInstrumentScale = (scale) => {
+      scale = [
+        "D2",
+        "D#2",
+        "E2",
+        "F2",
+        "F#2",
+        "G2",
+        "G#2",
+        "A2",
+        "A#2",
+        "B2",
+        "C3",
+        "C#3",
+        "D3",
+        "D#3",
+        "E3",
+        "F3",
+        "F#3",
+        "G3",
+        "G#3",
+        "A3",
+        "A#3",
+        "B3",
+        "C4",
+        "C#4",
+        "D4",
+        "D#4",
+        "E4",
+        "F4",
+        "F#4",
+        "G4",
+        "G#4",
+        "A4",
+        "A#4",
+        "B4",
+      ].reverse();
+      this.setState({ availableNotes: scale });
+    };
+
+    setDrumScale = (drumScale) => {
+      drumScale = ["E2", "D2"];
+
+      // Update the state to set the drum scale
+      this.setState({ availableDrumNotes: drumScale });
+    };
+
   render() {
     const {
       loading,
