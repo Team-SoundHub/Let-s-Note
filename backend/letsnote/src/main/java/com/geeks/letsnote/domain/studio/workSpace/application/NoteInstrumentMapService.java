8e9fa5f58d8e42595e1728e44491e9c383b44e3d
garden <gkdlqkql6@naver.com>
2024-02-04 18:21:18 +0900

    feat : loop 최대시 삭제 로직과 loop 생성 service 구현

--- backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/application/NoteInstrumentMapService.java
+++ backend/letsnote/src/main/java/com/geeks/letsnote/domain/studio/workSpace/application/NoteInstrumentMapService.java
@@ -1,9 +1,11 @@
 package com.geeks.letsnote.domain.studio.workSpace.application;
 
 import com.geeks.letsnote.domain.studio.instrument.Instrument;
-import com.geeks.letsnote.domain.studio.workSpace.dto.RequestNotes;
 import com.geeks.letsnote.domain.studio.workSpace.dto.ResponseNotes;
 import com.geeks.letsnote.global.network.dto.SocketRequest;
+import com.geeks.letsnote.global.network.dto.SocketResponse;
+
+import java.util.List;
 
 public interface NoteInstrumentMapService {
     ResponseNotes.Notes getAllInstrumentNoteBySpaceId (String spaceId, Instrument instrument);
@@ -11,5 +13,7 @@ public interface NoteInstrumentMapService {
     void clickNoteMap(SocketRequest.Coordinate coordinate);
     void createWorkspaceInstrumentMap(String spaceId);
 
-    void deleteNoteBySpaceIdAndInstrument(SocketRequest.SpaceInstrument spaceInstrument);
+    List<SocketResponse.Note> deleteNoteBySpaceIdAndInstrument(SocketRequest.SpaceInstrument spaceInstrument);
+
+    String getInstrumentMapBySpaceIdAndInstrument(String spaceId, Instrument instrument);
 }
