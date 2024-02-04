7d4b69af45cd4e33114c3325fa8d5390a2560274
Vacayy <125976021+Vacayy@users.noreply.github.com>
2024-02-05 06:28:30 +0900

    Frontend/악보UI변경 (#170)
    * 커서 주석 해제
    
    * 악보 UI 조금 손 보다가 중단

--- frontend/src/components/WorkSpace/ImagePreviewModal.jsx
+++ frontend/src/components/WorkSpace/ImagePreviewModal.jsx
@@ -4,19 +4,19 @@ import { Button, Modal } from 'flowbite-react';
 const ImagePreviewModal = ({ image_url, onClose }) => {
     return (
         <Modal show={Boolean(image_url)} onClose={onClose}>
-            <Modal.Body>
-                <div className="flex justify-center items-center">
-                    {/* 이미지를 중앙에 배치하고, 모달 크기에 맞추어 조절 */}
-                    <img 
-                        src={image_url} 
+
+            <div className={`fixed right-0 top-0 w-1/3 h-full bg-white shadow-lg z-10`}>
+                <div className="flex justify-end p-4">
+                    <button onClick={onClose} className="text-xl font-bold">X</button>
+                </div>
+                <div className="flex justify-center items-center h-full p-4">
+                    <img
+                        src={image_url}
                         alt="Selected Image"
                         className="max-w-full max-h-[calc(100vh-4rem)] object-contain" // 높이를 브라우저 뷰포트의 100% - 모달의 마진으로 설정
                     />
                 </div>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={onClose}>Close</Button>
-            </Modal.Footer>
+            </div>
         </Modal>
     );
 };
