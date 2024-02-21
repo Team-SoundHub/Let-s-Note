46bc54595d7722b67b7917cce7cbcaabd129fecb
1stApplePie <chanwoo6422@naver.com>
2024-02-21 21:19:33 +0900

    Frontend/feat/스냅샷 헤더 UI 수정

--- frontend/src/containers/Snapshot/SnapshotHeader.jsx
+++ frontend/src/containers/Snapshot/SnapshotHeader.jsx
@@ -30,7 +30,7 @@ const fadeOut = keyframes`
 `;
 
 const Header = styled.div`
-  background-color: #f3f3f3;
+  background-color: #49c5b6;
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
@@ -86,7 +86,43 @@ const SnapshotHeader = ({ onOpenModal, fromMyPage }) => {
 
   return (
     <Header>
-      <button onClick={handleGoBack}> ⬅️ </button>
+      <button onClick={handleGoBack}>
+        <svg
+          fill="#ffffff"
+          width="1.5rem"
+          height="1.5rem"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 8 8"
+          enable-background="new 0 0 8 8"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <rect
+              x="-0.226"
+              y="4.614"
+              transform="matrix(0.7071 0.7071 -0.7071 0.7071 4.4884 -0.1417)"
+              width="5.283"
+              height="1.466"
+            ></rect>{" "}
+            <rect x="1.607" y="3.161" width="6.375" height="1.683"></rect>{" "}
+            <rect
+              x="-0.233"
+              y="1.921"
+              transform="matrix(0.7069 -0.7073 0.7073 0.7069 -1.1708 2.4817)"
+              width="5.284"
+              height="1.465"
+            ></rect>{" "}
+          </g>
+        </svg>
+      </button>
       <ButtonContainer>
         <ShareButton />
       </ButtonContainer>
