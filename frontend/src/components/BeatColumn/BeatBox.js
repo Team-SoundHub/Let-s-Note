037a22d13210ad37a6577e003c9a93f61b587462
songarden <gkdlqkql06@gmail.com>
2024-02-21 23:55:52 +0900

    chore : beatbox를 margin으로 구분선을 그은것 처럼 보이게 하던 방식을 실제로 구분선을 아래만 주는 방식으로 변경

--- frontend/src/components/BeatColumn/BeatBox.js
+++ frontend/src/components/BeatColumn/BeatBox.js
@@ -11,7 +11,7 @@ const Container = styled.div`
     }
   }
   flex: 1;
-  margin: 0.05rem;
+  border-bottom: 0.05rem solid skyblue;
   background: ${(props) => {
     const activeInstrumentCount = props.active.filter(
       (isActive) => isActive
