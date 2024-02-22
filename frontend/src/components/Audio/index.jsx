806fb8fe2ae99aeee95540ba57eedeca7dd2aa5c
Vacayy <juyeong.kim.201@gmail.com>
2024-02-22 17:53:47 +0900

    feat: 스피커 음소거 한 유저 css 적용

--- frontend/src/components/Audio/index.jsx
+++ frontend/src/components/Audio/index.jsx
@@ -1,54 +1,73 @@
 import React, { useEffect, useRef, useState } from 'react';
-import tw from 'tailwind-styled-components';
-import styled from 'styled-components';
-import Button from '../common/Button';
-import cx from "classnames";
+import styled, { css } from 'styled-components';
+import soundMuteIcon from '../../assets/workspace/soundMute.png';
 
-const Container = tw.div`
-	flex
-	items-center
-	space-x-2
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem; /* space-x-2 equivalent */
 `;
 
 const VideoContainer = styled.audio`
-	height : 0px;
-	width: 0px;
+  height: 0px;
+  width: 0px;
 `;
 
-const UserLabel = tw.button`
-	bg-blue-100
-	text-blue-800
-	text-sm
-	font-medium
-	me-2
-	px-5
-	py-2.5
-	rounded-xl
-	dark:bg-blue-900
-	dark:text-blue-300
+const UserLabel = styled.button`
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  background-color: #b1f1b0; 
+  /* color: #1e40af;  */
+  color: #444444; 
+  font-size: 0.875rem; 
+  font-weight: 500;   
+  max-height: 2.1rem;
+  padding: 0.625rem 1.25rem; 
+  border: 0.1rem solid #71ff4a;
+  border-radius: 0.75rem; 
+  transition: background-color 0.2s ease-in-out;
+
+  ${props => props.muted && css`
+    background-color: #9ca3af; 
+	border: 0.1rem solid #ababab;
+	max-height: 2.1rem;
+    color: #f3f4f6; 	
+  `}
+
+  &:hover {
+    background-color: #93c5fd; 
+  }
 `;
+
+const muteIcon = styled.img`
+	width: 3rem;
+	height: 3rem;
+	
+`
 
 const Audio = ({ stream, muted, username, users }) => {
-	const ref = useRef(null);
-	const [isMuted, setIsMuted] = useState(false);
+  const ref = useRef(null);
+  const [isMuted, setIsMuted] = useState(muted);    
 
-    const handleMute = () => {
-        setIsMuted(!isMuted);
-    }
+  const handleMute = () => {
+    setIsMuted(!isMuted);    
+  }
 
-	useEffect(() => {
-		if (ref.current) ref.current.srcObject = stream;
-		if (muted) setIsMuted(muted);
-		
-	}, [stream, muted, users]);
+  useEffect(() => {
+    if (ref.current) ref.current.srcObject = stream;
+    if (muted !== isMuted) setIsMuted(muted);
+    
+  }, [stream, muted, users]);
 
-	return (
-		<Container>
-            <UserLabel onClick={handleMute}>
-                {username}</UserLabel>
-            <VideoContainer ref={ref} muted={isMuted} autoPlay volume="0.5"/>
-		</Container>
-	);
+  return (
+    <Container>
+      <UserLabel onClick={handleMute} muted={isMuted}>
+        {username} 
+      </UserLabel>
+      <VideoContainer ref={ref} muted={isMuted} autoPlay volume="0.5" />
+    </Container>
+  );
 };
 
 export default Audio;
