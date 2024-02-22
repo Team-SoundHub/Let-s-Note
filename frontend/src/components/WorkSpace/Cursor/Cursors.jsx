8f1a214e791a641aa4902f1937f5161c6e3f36ed
Vacayy <125976021+Vacayy@users.noreply.github.com>
2024-02-22 16:34:07 +0900

    feat: 커서 삭제 구현 마무리 (#312)

--- frontend/src/components/WorkSpace/Cursor/Cursors.jsx
+++ frontend/src/components/WorkSpace/Cursor/Cursors.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import cursorImage from '../../../assets/workspace/cursor.png';
+import { clearCursorPosition } from '../../../app/slices/cursorSlice';
 
 const Cursors = () => {    
+    const dispatch = useDispatch();
     const { positions } = useSelector((state) => state.cursor);
+
+    useEffect(() => {
+        return () => {
+            dispatch(clearCursorPosition());
+            console.log("커서 정보 삭제");
+        }
+    },[])
 
     return (
         <>
